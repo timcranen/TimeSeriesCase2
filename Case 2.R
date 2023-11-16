b8742f766224e0762856fe95cd7badd1a3adbf45
timcranen <150593987+timcranen@users.noreply.github.com>
2023-11-16 18:56:13 +0100

    Update Case 2.R
    I added the part where we want to determine the lag order of the VAR process. First specify the lag order we want to run (line 60), create numeric vectors to store the IC's (lines 63 and 64). for loop that runs the var for all different horizons and store the corresponding IC's in the numeric vector. choose the lowest IC in lines 75 and 76.

--- Case 2.R
+++ Case 2.R
@@ -1,6 +1,6 @@
 rm(list=ls())
 library(tseries)
-
+library(vars)
 # Set working directory
 setwd("~/Documents/Time series econometrics")
 
@@ -36,17 +36,45 @@ adf.test(diffFEDFUNDS)
 
 # Take for the CPI
 diffCPIAUCSL <- diff(diff(log(CPIAUCSL$CPIAUCSL)))
-plot(diffCPIAUCSL, type="l")
+plot(x = diffCPIAUCSL, type="l")
 
 # Augmented Dickey-Fuller test to test for stationarity
 adf.test(diffCPIAUCSL)
 
 # Take the log difference for the industrial production index
 diffINDPRO <- diff(log(INDPRO$INDPRO))
-plot(diffINDPRO, type="l")
+plot(x = diffINDPRO, type="l")
 
 # Augmented Dickey-Fuller test to test for stationarity
 adf.test(diffINDPRO)
+
+
+# Determining the lag order of the VAR(p) process
+data <- data.frame(
+  cpi = CPIAUCSL$CPIAUCSL,
+  fed = FEDFUNDS$FEDFUNDS,
+  ind = INDPRO$INDPRO
+)
+
+# Iterate over different lag orders
+lag_orders <- 1:20  # Adjust the range based on your data and context
+
+# Store AIC or BIC values for each lag order
+aic_values <- numeric(length(lag_orders))
+bic_values <- numeric(length(lag_orders))
+
+# Run the VAR model over the different lag orders and store the IC's
+for (i in seq_along(lag_orders)) {
+  current_lag <- lag_orders[i]
+  model <- VAR(data, p = current_lag, type = "const", lag.max = current_lag, season = 12)
+  aic_values[i] <- AIC(model)
+  bic_values[i] <- BIC(model)
+}
+
+# Choose the best IC
+best_aic_order <- lag_orders[which.min(aic_values)]
+best_bic_order <- lag_orders[which.min(bic_values)]
+
 
 
 
